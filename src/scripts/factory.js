82c09120fd63589261b398a971512288ef72207e
Allie <allison.tpttn@gmail.com>
2019-08-21 20:26:54 -0500

    able to render form but no drop down option

--- src/scripts/factory.js
+++ src/scripts/factory.js
@@ -6,16 +6,27 @@ const factory = Object.create({
                 Point of Interest
             </header>
             <div class="poi--fieldsets">
-                <input type="hidden" id="eventId" value="${interest ? interest.id : ""}" />
+                <input type="hidden" id="interestId" value="${interest ? interest.id : ""}" />
                      Location: <input id="poiLocation" value="${interest ? interest.location : ""}" type="text" name="poiLocation" class="poi__form--input"><br>
                      Name: <input id="poiName" value="${interest ? interest.name : ""}" type="text" name="poiName" class="poi__form--input"><br>
                      description: <input id="poiDescription" value="${interest ? interest.description : ""}" type="text" name="poiDescriotion" class="poi__form--input"><br>
-                     cost: <input id="poicost" value="${interest ? interest.cost : ""}" type="text" name="poiCost" class="poi__form--input"><br>
-                     review: <input id="poireview" value="${interest ? interest.review : ""}" type="text" name="poiReview" class="poi__form--input"><br>
-                <button id="poi__form--submit">Submit</button>
+                     cost: <input id="poiCost" value="${interest ? interest.cost : ""}" type="text" name="poiCost" class="poi__form--input"><br>
+                     review: <input id="poiReview" value="${interest ? interest.review : ""}" type="text" name="poiReview" class="poi__form--input"><br>
+                <button id="poi__form__submit">Submit</button>
             </div>
         </section>
     `
-    }
+    },
+    // function to create poi object
+    createPOIInputObj: (placeIdInput, nameInput, descriptionInput, costInput, reviewInput) => {
+        return {
+            placeId: placeIdInput,
+            name: nameInput,
+            description: descriptionInput,
+            cost: costInput,
+            review: reviewInput,
+        }
+    },
+
 });
 export default factory;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,27 @@ import data from "./data.js";
 import factory from "./factory.js";
 import dom from "./dom.js";
 
-//  event handler on add poi button
-const addPOIButton = document.querySelector("#addPOI__button");
-// event listener for add new poi
-addPOIButton.addEventListener("click", event => {
-    // render the add poi form when button is clicked
-    dom.renderPOIFormToDom();
-});
+const displayInterests = () => {
+    //  event handler on add poi button
+    const addPOIButton = document.querySelector("#addPOI__button");
+    // event listener for add new poi
+    addPOIButton.addEventListener("click", event => {
+        // render the add poi form when button is clicked
+        dom.renderPOIFormToDom();
+        // when sumbmit button is clicked
+        const submitbutton = document.querySelector("#poi__form__submit")
+        submitbutton.addEventListener("click", event => {
+            const poiLocation = document.querySelector("#poiLocation");
+            const poiName = document.querySelector("#poiName");
+            const poiDescription = document.querySelector("#poiDescription");
+            const poiCost = document.querySelector("#poiost");
+            const poiReview = document.querySelector("#poiReview");
+            const createPOIObj = factory.createPOIInputObj(
+                
+            )
+        });
+    });
+
+}
+
+displayInterests();
