da0fd8643be27cd34f55d55ea703b1f64d2aa60a
Allie <allison.tpttn@gmail.com>
2019-08-22 09:08:09 -0500

    able to post to json

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,4 +1,23 @@
 const data = Object.create({
+        postNewInterest: (interestObj) => {
+          return fetch("http://localhost:3000/interests", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(interestObj)
+          })
+            .then(newInterest => newInterest.json())
+        },
+        // deleteInterest: (id) => {
+        //     return fetch(`http://localhost:8088/interests/${id}`, {
+        //       method: "DELETE",
+        //       headers: {
+        //         "Content-Type": "application/json"
+        //       },
+        //     })
+        //       .then(deletedInterest => deletedInterest.json())
+        //   },
 });
 
 export default data;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,14 +14,17 @@ const displayInterests = () => {
         const submitbutton = document.querySelector("#poi__form__submit")
         submitbutton.addEventListener("click", event => {
             const poiLocation = document.querySelector("#poiLocation__DropDown");
-            const poiId = poiLocation.options[poiLocation.selectedIndex].id;
+            const placeId = parseInt(poiLocation.options[poiLocation.selectedIndex].id);
+            console.log("placeId:", placeId);
             const poiName = document.querySelector("#poiName");
             const poiDescription = document.querySelector("#poiDescription");
-            const poiCost = document.querySelector("#poiost");
+            const poiCost = document.querySelector("#poiCost");
             const poiReview = document.querySelector("#poiReview");
             const createPOIObj = factory.createPOIInputObj(
-                poiId, poiName.value, poiDescription.value, poiCost.value, poiReview.value
+                placeId, poiName.value, poiDescription.value, poiCost.value, poiReview.value
             );
+            console.log(createPOIObj)
+            data.postNewInterest(createPOIObj)
         });
     });
 
